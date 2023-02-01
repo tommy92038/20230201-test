5288790dd803671fbb44bf8ba2f37c57be5b82f0
unknown <tommy92038@gmail.com>
2023-02-02 02:06:56 +0800

    create employeedata table

---
+++ 20230201永慶/Models/EmployeeData.cs
@@ -0,0 +1,11 @@
+﻿namespace _20230201永慶.Models
+{
+    public class EmployeeData
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+    }
+}
