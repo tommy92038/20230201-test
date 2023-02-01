d57e857914ef464390ad5a9f235b8ce7161e2a68
Tommy <tommy92038@gmail.com>
2023-02-02 05:09:36 +0800

    create employee data

--- 20230201永慶/Controllers/EmployeeDataController.cs
+++ 20230201永慶/Controllers/EmployeeDataController.cs
@@ -6,13 +6,34 @@ namespace _20230201永慶.Controllers
 {
     public class EmployeeDataController : Controller
     {
+        
         public IActionResult Index()
         {
             return View();
         }
+        public IActionResult getEmployeeData()
+        {
+            DBM dbm = new DBM();
+            SqlConnection conn = new SqlConnection(dbm.ConnStr);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM EmployeeData", conn);
+            SqlDataReader dr = cmd.ExecuteReader();
+            List<EmployeeData>Employees= new List<EmployeeData>();
+            while(dr.Read())
+            {
+                Employees.Add(new EmployeeData()
+                {
+                    Id = (string)dr.GetValue(0),
+                    Name = (string)dr.GetValue(1),
+                    Address = (string)dr.GetValue(2),
+                    Email = (string)dr.GetValue(3),
+                });
+            }
+            return View(Employees);
+        }
         public IActionResult Create(EmployeeData EmployeeData)
         {
-            DBM dbm= new DBM();
+            DBM dbm = new DBM();
             SqlConnection conn=new SqlConnection(dbm.ConnStr);
             conn.Open();
             SqlCommand cmd = new SqlCommand("INSERT INTO EmployeeData(Id,Name,Address,Email)VALUES(@Id,@Name,@Address,@Email) ", conn);
@@ -23,11 +44,7 @@ namespace _20230201永慶.Controllers
             SqlDataReader dr=cmd.ExecuteReader();
             dr.Dispose();
             conn.Close();
-            return View("Index");
-        }
-        public IActionResult getEmployeeData()
-        {
-            return View();
+            return RedirectToAction("getEmployeeData");
         }
     }
 }
