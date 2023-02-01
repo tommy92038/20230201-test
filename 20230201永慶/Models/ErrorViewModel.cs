b82cd8c74e0ae48e87c544ef6bbfda322afe1af4
Tommy <tommy.chen@cloudysys.com>
2023-02-01 23:20:22 +0800

    create project

---
+++ 20230201永慶/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _20230201永慶.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
